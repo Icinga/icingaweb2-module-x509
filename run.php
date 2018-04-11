9e34ba0772b3f5537ed152e93e62ce6a35521f6d
Alexander A. Klimov <alexander.klimov@icinga.com>
2018-04-11 16:59:38 +0200

    Fetch ipl/sql from GitHub via Composer

--- run.php
+++ run.php
@@ -1,10 +1,4 @@
 <?php
 /* X509 module | (c) 2018 Icinga Development Team | GPLv2+ */
 
-use Icinga\Application\Icinga;
-use ipl\Loader\CompatLoader;
-
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/ipl/lib/ipl/Loader/CompatLoader.php';
-
-CompatLoader::delegateLoadingToIcingaWeb(Icinga::app());

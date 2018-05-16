86730423e781cc5be1a417a6f11fee6073e0c75e
Eric Lippmann <eric.lippmann@icinga.com>
2018-05-16 13:47:36 +0200

    Add icingacli x509 verify

---
+++ application/clicommands/VerifyCommand.php
@@ -0,0 +1,27 @@
+<?php
+/* Icinga Web 2 X.509 module | (c) 2018 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Module\X509\Clicommands;
+
+use Icinga\Application\Logger;
+use Icinga\Module\X509\Command;
+use Icinga\Module\X509\CertificateUtils;
+
+class VerifyCommand extends Command
+{
+    /**
+     * Verify all currently collected X.509 certificates
+     *
+     * USAGE:
+     *
+     *   icingacli x509 verify
+     */
+    public function indexAction()
+    {
+        $db = $this->getDb();
+
+        $verified = CertificateUtils::verifyCertificates($db);
+
+        Logger::info("Checked %d certificate chain%s.", $verified, $verified !== 1 ? 's' : '');
+    }
+}

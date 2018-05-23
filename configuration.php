c527f100dcc4656fc3ad3f8e383119735b8d0cc0
Eric Lippmann <eric.lippmann@icinga.com>
2018-05-23 14:28:06 +0200

    Add menu

--- configuration.php
+++ configuration.php
@@ -2,6 +2,22 @@
 
 /** @var \Icinga\Application\Modules\Module $this */
 
+$section = $this->menuSection(N_('X.509'), array(
+    'icon'      => 'check',
+    'url'       => 'x509/dashboard',
+    'priority'  => 40
+));
+
+$section->add(N_('Certificate Overview'), array(
+    'url'       => 'x509/certificates',
+    'priority'  => 10
+));
+
+$section->add(N_('Certificate Usage'), array(
+    'url'       => 'x509/usage',
+    'priority'  => 20
+));
+
 $this->provideConfigTab('backend', array(
     'title' => $this->translate('Configure the database backend'),
     'label' => $this->translate('Backend'),

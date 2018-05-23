deaaa99a0fc7535597db094a2d1036d81900a32a
Eric Lippmann <eric.lippmann@icinga.com>
2018-05-23 16:00:31 +0200

    View chains

---
+++ application/controllers/ChainController.php
@@ -0,0 +1,72 @@
+<?php
+/* X509 module | (c) 2018 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Module\X509\Controllers;
+
+use Icinga\Module\X509\CertificateDetails;
+use Icinga\Module\X509\ChainDetails;
+use Icinga\Module\X509\Controller;
+use ipl\Html\Attribute;
+use ipl\Html\Html;
+use ipl\Html\HtmlDocument;
+use ipl\Sql;
+
+class ChainController extends Controller
+{
+    public function indexAction()
+    {
+        $certId = $this->params->getRequired('cert');
+        $targetId = $this->params->getRequired('target');
+
+        $conn = $this->getDb();
+
+        $certSelect = (new Sql\Select())
+            ->from('x509_target t')
+            ->columns('*')
+            ->join('x509_certificate_chain ch', 'ch.target_id = t.id')
+            ->join('x509_certificate_chain_link chc', 'chc.certificate_chain_id = ch.id')
+            ->join('x509_certificate c', 'c.id = chc.certificate_id')
+            ->where(['chc.order = ?' => 0, 'chc.certificate_id = ?' => $certId, 't.id = ?' => $targetId]);
+
+        $cert = $conn->select($certSelect)->fetch();
+
+        if ($cert === false) {
+            $this->httpNotFound($this->translate('Certificate not found.'));
+        }
+
+        $chainInfo = Html::tag('div');
+        $chainInfo->add(Html::tag('dl', [
+            Html::tag('dt', $this->translate('Host')),
+            Html::tag('dd', ! empty($cert['sni_name']) ? $cert['sni_name'] : gethostbyaddr(inet_ntop($cert['ip']))),
+            Html::tag('dt', $this->translate('IP')),
+            Html::tag('dd', inet_ntop($cert['ip'])),
+            Html::tag('dt', $this->translate('Port')),
+            Html::tag('dd', $cert['port'])
+        ]));
+
+        $valid = Html::tag('div', ['class' => 'cert-chain']);
+
+        if ($cert['valid'] === 'yes') {
+            $valid->getAttributes()->add('class', '-valid');
+            $valid->add(Html::tag('p', $this->translate('Certificate chain is valid.')));
+        } else {
+            $valid->getAttributes()->add('class', '-invalid');
+            $valid->add(Html::tag('p', sprintf($this->translate('Certificate chain is invalid: %s.'), $cert['invalid_reason'])));
+        }
+
+        $this->setTitle($this->translate('Certificate Chain'));
+
+        $chainSselect = (new Sql\Select())
+            ->from('x509_certificate c')
+            ->columns('*')
+            ->join('x509_certificate_chain_link ccl', 'ccl.certificate_id = c.id')
+            ->join('x509_certificate_chain cc', 'cc.id = ccl.certificate_chain_id')
+            ->where(['cc.id = ?' => $cert['certificate_chain_id']])
+            ->orderBy('ccl.order');
+
+        $this->view->chain = (new HtmlDocument())
+            ->add($chainInfo)
+            ->add($valid)
+            ->add((new ChainDetails())->setData($conn->select($chainSselect)));
+    }
+}
---
+++ library/X509/ChainDetails.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Icinga\Module\X509;
+
+use Icinga\Web\Url;
+use ipl\Html\Html;
+use ipl\Translation\Translation;
+
+/**
+ * Table widget to display X.509 chain details
+ */
+class ChainDetails extends DataTable
+{
+    use Translation;
+
+    protected $defaultAttributes = [
+        'class' => 'cert-table common-table table-row-selectable',
+        'data-base-target' => '_next'
+    ];
+
+    public function createColumns()
+    {
+        return [
+            [
+                'attributes' => ['class' => 'icon-col'],
+                'renderer' => function () {
+                    return Html::tag('i', ['class' => 'icon x509-icon-cert']);
+                }
+            ],
+
+            'version' => [
+                'attributes' => ['class' => 'version-col'],
+                'renderer' => function ($version) {
+                    return Html::tag('div', ['class' => 'badge'], $version);
+                }
+            ],
+
+            'subject' => [
+                'label' => $this->translate('Subject')
+            ],
+
+            'ca' => [
+                'attributes' => ['class' => 'icon-col'],
+                'renderer' => function ($ca) {
+                    if ($ca === 'no') {
+                        return null;
+                    }
+
+                    return Html::tag(
+                        'i',
+                        ['class' => 'icon x509-icon-ca', 'title' => $this->translate('Is Certificate Authority')]
+                    );
+                }
+            ],
+
+            'self_signed' => [
+                'attributes' => ['class' => 'icon-col'],
+                'renderer' => function ($selfSigned) {
+                    if ($selfSigned === 'no') {
+                        return null;
+                    }
+
+                    return Html::tag(
+                        'i',
+                        ['class' => 'icon x509-icon-self-signed', 'title' => $this->translate('Is Self-Signed')]
+                    );
+                }
+            ],
+
+            'trusted' => [
+                'attributes' => ['class' => 'icon-col'],
+                'renderer' => function ($trusted) {
+                    if ($trusted === 'no') {
+                        return null;
+                    }
+
+                    return Html::tag(
+                        'i',
+                        ['class' => 'icon icon-thumbs-up', 'title' => $this->translate('Is Trusted')]
+                    );
+                }
+            ],
+
+            'signature_algo' => [
+                'label' => $this->translate('Signature Algorithm'),
+                'renderer' => function ($algo, $data) {
+                    return "$algo {$data['signature_hash_algo']}";
+                }
+            ],
+
+            'pubkey_algo' => [
+                'label' => $this->translate('Public Key'),
+                'renderer' => function ($algo, $data) {
+                    return "$algo {$data['pubkey_bits']}";
+                }
+            ],
+
+            'valid_to' => [
+                'attributes' => ['class' => 'expiration-col'],
+                'label' => $this->translate('Expires'),
+                'renderer' => function ($to, $data) {
+                    return new ExpirationWidget($data['valid_from'], $to);
+                }
+            ]
+        ];
+    }
+
+    protected function renderRow($row)
+    {
+        $tr = parent::renderRow($row);
+
+        $url = Url::fromPath('x509/certificate', ['cert' => $row['certificate_id']]);
+
+        $tr->getAttributes()->add(['href' => $url->getAbsoluteUrl()]);
+
+        return $tr;
+    }
+}

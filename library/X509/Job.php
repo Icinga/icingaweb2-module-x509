014cbd81866414309fbd5e5ba53a16192975ba2d
Alexander A. Klimov <alexander.klimov@icinga.com>
2019-01-16 10:46:00 +0100

    Job#startNextTarget(): correct SQL UPDATE filter
    refs #21

--- library/X509/Job.php
+++ library/X509/Job.php
@@ -256,7 +256,7 @@ class Job
                 $this->db->update(
                     'x509_target',
                     ['latest_certificate_chain_id' => null],
-                    ['ip = ?' => static::binary($target->ip), 'port = ?' => $target->port, 'hostname = ?' => $target->hostname ]
+                    ['ip = ?' => static::binary($target->ip), 'port = ?' => $target->port]
                 );
 
                 $this->finishTarget();

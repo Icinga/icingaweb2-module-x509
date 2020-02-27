8cda92bbea03660b45bb5063b1acfaf033b1d875
Eric Lippmann <eric.lippmann@icinga.com>
2020-02-27 14:20:02 +0100

    Schema: Use signed bigints for valid_from and valid_to
    This now supports certificates that claim to be valid before 1970-01-01
    (negative valid_from_t) and certificates where valid before is BIGGER
    than valid to. wtf.

--- etc/schema/mysql-upgrade/v1.1.0.sql
+++ etc/schema/mysql-upgrade/v1.1.0.sql
@@ -1,2 +1,4 @@
 ALTER TABLE x509_target DROP INDEX x509_idx_target_ip_port_hostname;
 ALTER TABLE x509_target ADD INDEX x509_idx_target_ip_port (ip, port);
+ALTER TABLE x509_certificate MODIFY COLUMN valid_from bigint(20) NOT NULL;
+ALTER TABLE x509_certificate MODIFY COLUMN valid_to bigint(20) NOT NULL;

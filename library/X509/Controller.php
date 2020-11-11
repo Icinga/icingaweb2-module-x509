aba39ac63145969eddcf89ba41fc77474c0c2037
Eric Lippmann <eric.lippmann@icinga.com>
2020-11-11 12:35:28 +0100

    Set MYSQL_ATTR_INIT_COMMAND only when connecting to a MySQL database
    If PDO_MYSQL is not enabled / installed and a connection to a
    non-MySQL database was established, this failed with an exception:
    
    Uncaught Error: Undefined class constant 'MYSQL_ATTR_INIT_COMMAND'

--- library/X509/Controller.php
+++ library/X509/Controller.php
@@ -25,11 +25,14 @@ class Controller extends \Icinga\Web\Controller
             $this->Config()->get('backend', 'resource')
         ));
 
-        $config->options = [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION SQL_MODE='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE"
-                . ",ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'"
+        $options = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
+        if ($config->db === 'mysql') {
+            $options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET SESSION SQL_MODE='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE"
+                . ",NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'";
+        }
+        $config->options = $options;
 
         $conn = new Sql\Connection($config);
 

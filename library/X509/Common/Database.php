8b975ca3fc87761c8a65123ac55a21d8da2ef383
Yonas Habteab <yonas.habteab@icinga.com>
2022-09-22 13:56:11 +0200

    Introduce trait `Database`

---
+++ library/X509/Common/Database.php
@@ -0,0 +1,37 @@
+<?php
+
+/* Icinga Web 2 X.509 Module | (c) 2022 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\X509\Common;
+
+use Icinga\Data\ResourceFactory;
+use ipl\Sql;
+use PDO;
+
+trait Database
+{
+    /**
+     * Get the connection to the X.509 database
+     *
+     * @return  Sql\Connection
+     */
+    protected function getDb(array $options = [])
+    {
+        $config = new Sql\Config(ResourceFactory::getResourceConfig(
+            $this->Config()->get('backend', 'resource')
+        ));
+
+        if (! isset($options[PDO::ATTR_DEFAULT_FETCH_MODE])) {
+            $options[PDO::ATTR_DEFAULT_FETCH_MODE] = PDO::FETCH_OBJ;
+        }
+
+        if ($config->db === 'mysql') {
+            $options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET SESSION SQL_MODE='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE"
+                . ",NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'";
+        }
+
+        $config->options = $options;
+
+        return new Sql\Connection($config);
+    }
+}

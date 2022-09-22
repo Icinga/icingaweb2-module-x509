b8c9856e14812b37202d35ead3f4ad3003a153fd
Yonas Habteab <yonas.habteab@icinga.com>
2022-09-22 15:07:36 +0200

    Drop custom composer requirements

--- run.php
+++ run.php
@@ -2,7 +2,5 @@
 
 // Icinga Web 2 X.509 Module | (c) 2018 Icinga GmbH | GPLv2
 
-require_once __DIR__ . '/vendor/autoload.php';
-
 $this->provideHook('director/ImportSource', '\\Icinga\\Module\\X509\\ProvidedHook\\HostsImportSource');
 $this->provideHook('director/ImportSource', '\\Icinga\\Module\\X509\\ProvidedHook\\ServicesImportSource');

2119443368923a500545c2b1f2d9cfbf4f525950
Yonas Habteab <yonas.habteab@icinga.com>
2022-11-21 10:45:12 +0100

    Introduce `Ip` behaviour

---
+++ library/X509/Model/Behavior/Ip.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Icinga\Module\X509\Model\Behavior;
+
+use ipl\Orm\Contract\PropertyBehavior;
+
+class Ip extends PropertyBehavior
+{
+    public function fromDb($value, $key, $_)
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        $ipv4 = ltrim($value, "\0");
+        if (strlen($ipv4) === 4) {
+            $value = $ipv4;
+        }
+
+        return inet_ntop($value);
+    }
+
+    public function toDb($value, $key, $_)
+    {
+        if ($value === null || $value === '*' || ! ctype_print($value)) {
+            return $value;
+        }
+
+        return str_pad(inet_pton($value), 16, "\0", STR_PAD_LEFT);
+    }
+}

48503c6917cf24b8294a834d1acfc7292a8aab59
Yonas Habteab <yonas.habteab@icinga.com>
2023-02-22 11:34:37 +0100

    Remove `Controller::getDb()` override
    Now that we're using `ipl\Orm` everywhere, there's no need to set the fetch mode
    to anything other than `PDO::FETCH_OBJ`.

--- library/X509/Common/Database.php
+++ library/X509/Common/Database.php
@@ -16,16 +16,13 @@ trait Database
      *
      * @return  Sql\Connection
      */
-    protected function getDb(array $options = [])
+    protected function getDb(): Sql\Connection
     {
         $config = new Sql\Config(ResourceFactory::getResourceConfig(
             Config::module('x509')->get('backend', 'resource')
         ));
 
-        if (! isset($options[PDO::ATTR_DEFAULT_FETCH_MODE])) {
-            $options[PDO::ATTR_DEFAULT_FETCH_MODE] = PDO::FETCH_OBJ;
-        }
-
+        $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ];
         if ($config->db === 'mysql') {
             $options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET SESSION SQL_MODE='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE"
                 . ",NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'";

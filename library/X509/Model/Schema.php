1ca678bbeca530b756a559d6975def6bf2750d8f
Yonas Habteab <yonas.habteab@icinga.com>
2023-09-19 15:18:58 +0200

    Schema: Add class property type hints

--- library/X509/Model/Schema.php
+++ library/X509/Model/Schema.php
@@ -4,11 +4,21 @@
 
 namespace Icinga\Module\X509\Model;
 
+use DateTime;
 use ipl\Orm\Behavior\BoolCast;
 use ipl\Orm\Behavior\MillisecondTimestamp;
 use ipl\Orm\Behaviors;
 use ipl\Orm\Model;
 
+/**
+ * A database model for x509 schema version table
+ *
+ * @property int $id Unique identifier of the database schema entries
+ * @property string $version The current schema version of Icinga Web
+ * @property DateTime $timestamp The insert/modify time of the schema entry
+ * @property bool $success Whether the database migration of the current version was successful
+ * @property ?string $reason The reason why the database migration has failed
+ */
 class Schema extends Model
 {
     public function getTableName(): string
